e37c8de12df190fc8da236aa35fc4cc0f3b707b7
222e013 <nijibara@outlook.jp>
2022-10-18 17:11:39 +0900

    プロジェクト ファイルを追加します。

---
+++ cpp4/cpp4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "getVolumeSurface.h"
+using namespace std;
+void getVolumeSurface(double x, double y, double z,double &volume,double &surface) {
+	volume= x * y * z;
+	surface = 2 * (x * y + x * z + y * z);
+  }
+int main() {
+	double x, y, z;
+	double volume=0, surface=0;
+	cout << "幅を入力:";
+	cin >> x;
+	cout << "高さを入力:";
+	cin >> y;
+	cout << "奥行を入力:";
+	cin >> z;
+	getVolumeSurface(x, y, z,volume,surface);
+	cout << "体積は" << volume << endl << "表面積は" << surface << endl;
+	return 0;
+}
